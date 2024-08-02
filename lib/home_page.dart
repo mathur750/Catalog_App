abee22cc7b45f064164fdbf21ac8fd281bc37b86
mathur750 <saurabhmathur750@gmail.com>
2024-08-02 12:31:48 +0530

    Day-2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "codepur";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Catalog App'),
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcoe to 30 days of flutter"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
