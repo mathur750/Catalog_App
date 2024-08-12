a79173563791285729124fadec3ea564a2f5718a
mathur750 <saurabhmathur750@gmail.com>
2024-08-12 19:29:29 +0530

    day-5 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tut/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -6,53 +7,54 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.white,
-      child: Column(
-        children: [
-          Center(
-              child: Image.asset(
-            "assets/images/login.png.png",
-            fit: BoxFit.cover,
-          )),
-          const Text(
-            "Welcome",
-            style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                  child: Image.asset(
+                "assets/images/login.png.png",
+                fit: BoxFit.cover,
+              )),
+              const Text(
+                "Welcome",
+                style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 32.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Enter Username",
+                        labelText: "Username",
+                      ),
+                    ),
+                    TextFormField(
+                      obscureText: true, // use for hide the password.
+                      decoration: const InputDecoration(
+                        hintText: "Enter Password",
+                        labelText: "Password",
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30.0,
+                    ),
+                    ElevatedButton(
+                      child: Text("Login"),
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Enter Username",
-                    labelText: "Username",
-                  ),
-                ),
-                TextFormField(
-                  obscureText: true, // use for hide the password.
-                  decoration: const InputDecoration(
-                    hintText: "Enter Password",
-                    labelText: "Password",
-                  ),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    print("login succesfull");
-                  },
-                  child: Text("Login"),
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
