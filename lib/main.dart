423981a8031f866bc7dbad4a89fba9f7d0c00754
mathur750 <saurabhmathur750@gmail.com>
2024-08-16 12:05:19 +0530

    day-completed

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         // fontFamily: GoogleFonts.lato(),
         primaryTextTheme: GoogleFonts.latoTextTheme(),
       ),
-      initialRoute: "/",
+      initialRoute: MyRoutes.homeRoute,
       routes: {
         '/': (context) => LoginPage(),
         MyRoutes.homeRoute: (context) => HomePage(),
